50031201ef1735832af7d47007b4eef6d6c59fce
BlackBelt_Yogi <tanmay99@gmail.com>
2024-03-10 19:12:21 +0000

    config commit done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId =1154
+let accountEmail="tomcruise@gmail.com"
+var password="missionImpossible"
+city=" Los Angeles"
+
+// accountId = 2
+// Value of Constant cannot be changed. Use  "Cmd +/"  to comment
+// dfdfd
+console.log(accountId);
+
+
+/*
+
+Never use Var becuase of use of issue in block scope and functional scope
+*/
+accountEmail="Bettertomcruise@gmail.com"
+password="unchanged"
+city=" New York"
+console.table([accountId,accountEmail,password,city])
+console.table([ accountId, accountEmail, password, city])
